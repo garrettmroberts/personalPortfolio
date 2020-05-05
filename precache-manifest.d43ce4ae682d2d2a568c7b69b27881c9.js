037bdd5f935e03daf123ed06e50d5077e0da3849
Garrett Roberts <garrettmroberts@gmail.com>
2020-05-05 15:37:32 -0500

    Updates

---
+++ precache-manifest.d43ce4ae682d2d2a568c7b69b27881c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05460703b6dad4bd3430ec191ef8271b",
+    "url": "/personalPortfolio/index.html"
+  },
+  {
+    "revision": "68929b9594733842ab44",
+    "url": "/personalPortfolio/static/css/main.13f36762.chunk.css"
+  },
+  {
+    "revision": "b78c746acefe88462f22",
+    "url": "/personalPortfolio/static/js/2.1abb12d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personalPortfolio/static/js/2.1abb12d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68929b9594733842ab44",
+    "url": "/personalPortfolio/static/js/main.c68116f1.chunk.js"
+  },
+  {
+    "revision": "88d2c5ec775019ca9618",
+    "url": "/personalPortfolio/static/js/runtime-main.e559bfa3.js"
+  },
+  {
+    "revision": "7804f289d67d4211b9d5e22feb045e7d",
+    "url": "/personalPortfolio/static/media/parallax0.7804f289.jpg"
+  },
+  {
+    "revision": "ac2516d8944dc9445d1e04f1bf94658c",
+    "url": "/personalPortfolio/static/media/parallax1.ac2516d8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc3f94b67be97b544c40918011547e59.js"
+  "/personalPortfolio/precache-manifest.d43ce4ae682d2d2a568c7b69b27881c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personalPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
